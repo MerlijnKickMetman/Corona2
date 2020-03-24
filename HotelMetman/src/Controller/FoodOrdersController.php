0b0ed02733f146eb81b3635d29d6a3d67205d588
Merlijn Metman <merlijnkick16@gmail.com>
2020-03-24 15:49:13 +0100

    Update FoodOrdersController.php

--- HotelMetman/src/Controller/FoodOrdersController.php
+++ HotelMetman/src/Controller/FoodOrdersController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/food/orders")
+ * @Route("/foodorders")
  */
 class FoodOrdersController extends AbstractController
 {
